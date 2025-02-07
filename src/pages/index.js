d0740477561635a91caa29ac1a1fbcaacffb93c2
new name <tushig121@gmail.com>
2025-02-07 11:13:20 +0800

    2 sariin 7

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,51 @@
-import { useState } from "react";
+import React, { useState } from "react";
 
-export default function Home() {
-  const [click, setClick] = useState("sideeye");
-
-  let string = "bomb begin";
-  let number = 0;
-  let boolean = true;
+const UseStateTask = () => {
+  const [color, setColor] = useState("");
 
   return (
-    <div className="flex flex-col bg-yellow-50 min-h-screen ">
-      <button className="font-bold text-4xl border-[2px] border-black w" onClick={() => setClick("bombastic")}>
-        touch me
+    <div className={`flex space-x-4 w-full h-screen bg-${color}-800`}>
+      <p>Өнгө сонгох:</p>
+      <button
+        className="border bg-red-800 py-4 px-6"
+        onClick={() => {
+          setColor("red");
+        }}
+      >
+        Улаан
       </button>
-      <p>{click}</p>
+      {color == "red" ? null : (
+        <>
+  
+          <button
+            className="border bg-blue-800 py-4 px-6"
+            onClick={() => {
+              setColor("blue");
+            }}
+          >
+            Цэнхэр
+          </button>
+          <button
+            className="border bg-green-800 py-4 px-6"
+            onClick={() => {
+              setColor("green");
+            }}
+          >
+            Ногоон
+          </button>
+        </>
+      )}
+
+      <button
+        className="border bg-gray-800 text-white py-4 px-6"
+        onClick={() => {
+          setColor("");
+        }}
+      >
+        Refresh
+      </button>
     </div>
   );
-}
+};
+
+export default UseStateTask;
